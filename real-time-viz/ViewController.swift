7319d645873dabb218a5697f6860db7d6f406de5
Ryan Poplin <ryanpoplin@Ryans-Mac-mini.local>
2016-01-31 19:15:24 -0500

    setup xcode project

---
+++ real-time-viz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  real-time-viz
+//
+//  Created by Ryan Poplin on 1/31/16.
+//  Copyright © 2016 Luupy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ real-time-vizTests/real_time_vizTests.swift
@@ -0,0 +1,36 @@
+//
+//  real_time_vizTests.swift
+//  real-time-vizTests
+//
+//  Created by Ryan Poplin on 1/31/16.
+//  Copyright © 2016 Luupy. All rights reserved.
+//
+
+import XCTest
+@testable import real_time_viz
+
+class real_time_vizTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
